4232433759cf705628f0c3c9ebe0736ea42f371c
= <hellohi12465@gmail.com>
2025-06-10 02:34:41 -0700

    Completed step three. The given solution was a bit tricky because the prop parameter and Restaurant component hadn't been set up yet.

--- props-and-events/src/components/RestaurantsContainer.jsx
+++ props-and-events/src/components/RestaurantsContainer.jsx
@@ -1,7 +1,12 @@
+import Restaurant from "./Restaurant.jsx";
+import { restaurants } from "../data/data.js";
+
 function RestaurantsContainer() {
   return (
     <div className="restaurantContainer">
-
+      {restaurants.map((restaurant) => (
+        <Restaurant />
+      ))}
     </div>
   );
 }
